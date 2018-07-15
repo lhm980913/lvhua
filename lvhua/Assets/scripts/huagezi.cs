222e9a3c4de6f8060fed85a066fcea3c95afca73
Zoryx <15801610913@163.com>
2018-07-15 13:57:20 +0800

    no message

---
+++ lvhua/Assets/scripts/huagezi.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class huagezi : MonoBehaviour {
+    public float fenqu;
+    public GameObject xian;
+	// Use this for initialization
+	void Start () {
+        
+        for (int i = 0; i < fenqu / 2; i++)
+        {
+            Instantiate(xian, this.transform.position, Quaternion.Euler(new Vector3(0, 0, (360 / fenqu) * i)));
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
